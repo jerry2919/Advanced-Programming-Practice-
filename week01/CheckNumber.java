f99435e672144b5144ec2a4bfbf0af6dea28ed7d
jerry2919 <tonysharmila05@gmail.com>
2025-07-31 15:30:58 +0530

    check number

---
+++ week01/CheckNumber.java
@@ -0,0 +1,15 @@
+import java.util.scanner;
+public class CheckNumber9(){
+    public static void main(String[] args){
+        scanner input=new scanner(System.in);
+        system.out.println("enter the number:");
+        int num=input.nextInt();
+
+    if(num %2==0){
+         system.out.printlln("the number is even");
+    }else{
+        system.out.println("The number is odd");
+    }
+
+    }
+}
